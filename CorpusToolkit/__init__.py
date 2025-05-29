e590cd65c24c485c34728df94b9bafa4efbeca33
Morton Li <Morton.L@Outlook.com>
2025-05-29 18:36:48 +0800

    add sentence splitting functionality and integrate into the toolkit

--- CorpusToolkit/__init__.py
+++ CorpusToolkit/__init__.py
@@ -3,5 +3,6 @@ __version__ = "0.1.0"
 from .cleaner import Cleaner
 from .duplicates import DuplicateDetector
 from .scorer import fluency
+from .splitter import split_sentence
 
-__all__ = ['Cleaner', 'DuplicateDetector', 'fluency']
+__all__ = ['Cleaner', 'DuplicateDetector', 'fluency', 'split_sentence']
